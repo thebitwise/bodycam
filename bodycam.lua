6f683c94dc213071d947fecdb09ac2b015d3ed8d
bitwise <bitwisethegreat@gmail.com>
2023-11-11 21:47:26 -0500

    Update bodycam.lua

--- bodycam.lua
+++ bodycam.lua
@@ -7,7 +7,7 @@
 --$$$$$$$$\ $$ |  $$ |$$ | \$$ |\$$$$$$  |$$$$$$$$\ 
 --\________|\__|  \__|\__|  \__| \______/ \________|
 -- coded by Lance/stonerchrist on Discord
-
+pluto_use "0.5.0"
 util.require_natives("2944a", "g")
 
 local resources_dir =  filesystem.resources_dir() .. '\\bodycam\\'
